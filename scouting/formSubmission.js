ce6d3ce373f86a7fe995263d59fb172766d22233
lddtwo456 <72422028+lddtwo456@users.noreply.github.com>
2024-03-07 10:57:02 -0500

    Update formSubmission.js

--- scouting/formSubmission.js
+++ scouting/formSubmission.js
@@ -9,22 +9,11 @@ document.getElementById("dataForm").addEventListener("submit", function(event) {
   document.querySelector("#out").textContent = jsonString;
 });
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  var fieldContainer = document.getElementById("field-container");
-  var field = fieldContainer.querySelector("field");
+document.getElementById("#field").addEventListener("click", function(event) {
+  var fieldContainer = document.getElementById("#field-container");
+  var field = fieldContainer.querySelector("#field");
 
-  field.addEventListener("click", function(event) {
-    // get click pos rel to container
-    var rect = fieldContainer.getBoundingClientRect();
-    var offsetX = event.clientX - rect.left;
-    var offsetY = event.clientY - rect.top;
-
-    // get position of click rel to image
-    var imageX = offsetX / rect.width * field.naturalWidth;
-    var imageY = offsetY / rect.height * field.naturalHeight;
-
-    // out
-    document.querySelector("#xout").textContent = imageX;
-    document.querySelector("#yout").textContent = imageY;
-  })
+  // get click pos rel to container
+  document.querySelector("#xout").textContent = event.clientX;
+  document.querySelector("#yout").textContent = event.clientY;
 });
